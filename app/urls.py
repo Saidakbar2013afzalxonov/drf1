2f69a8f10b987339342f5a0b6b6d94ddf8f585cb
DostoevskiyRiuui <Hobbycool9@gmail.com>
2026-07-15 13:35:01 +0500

    CRUD

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import UserView
 urlpatterns = [
-    path("", UserView.as_view({'get':'list'})),
+    path("users/", UserView.as_view({'get':'list'})),
+    path("users/create/", UserView.as_view({'post':'create'})),
+    path("users/update/<int:pk>/", UserView.as_view({'put':'update', 'patch':'partial_update'})),
+    path("users/delete/<int:pk>/", UserView.as_view({'delete':'destroy'})),
 ]
